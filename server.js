074893374aed5742b51fdc54d91c90ff425e4afc
Dayher Marchan <dayhermarchan@gmail.com>
2022-02-26 16:58:27 -0300

    Conectado a db

--- server.js
+++ server.js
@@ -7,3 +7,16 @@ const app = express();
 const PORT = process.env.PORT;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGO_URI);
+
+const Cat = mongoose.model('Cat', { name: String });
+
+app.get('/api/kittens', async (req, res) => {
+    let kittens = await Cat.find();
+    res.json({
+        data:kittens
+    });
+});
