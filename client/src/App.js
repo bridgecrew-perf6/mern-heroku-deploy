0d853b8881109d698390dcb0cf00163e4e75f511
Dayher Marchan <dayhermarchan@gmail.com>
2022-02-27 10:35:59 -0300

    frontend conectado

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,46 @@
+import { useState, useEffect } from "react";
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+
+    const fecthPosts = async () => {
+       
+      try {
+        const kittens =  await fetch('/api/kittens');
+
+        const { data } = await kittens.json();
+        
+        setPosts(data);
+       
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fecthPosts();
+  }, 1);
+
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+       
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello Dayher!
         </p>
+
+        <div>
+            {posts.map((post) => (
+              <p>{post.name}</p>
+            ))}
+            
+          </div> 
         <a
           className="App-link"
           href="https://reactjs.org"
